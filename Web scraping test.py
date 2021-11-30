bb94319f4ae76e9e9dee35b92aaba556ce92d8e6
Secaution <jimmy.braun@gmx.de>
2021-11-30 22:42:54 +0100

    Adjusted code

--- Web scraping test.py
+++ Web scraping test.py
@@ -1,6 +1,14 @@
 ### This is a first web scraping draft ###
 
-from beautifulsoup4 import beautifulsoup
-from requests
-from daytime import daytime
+from bs4 import BeautifulSoup
+import requests
+from datetime import date
 
+today = date.today()
+url = "https://interaktiv.morgenpost.de/corona-virus-karte-infektionen-deutschland-weltweit/"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, "html.parser")
+result = soup.find(id="cmpbox2")
+
+print("Heutiges Datum (Jahr-Monat-Tag):", today)
+print("Neuinfektionen Corona heute in Deutschland:", result)
